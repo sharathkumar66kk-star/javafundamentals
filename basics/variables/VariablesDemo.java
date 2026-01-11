7a9997eb0eb38bef89e104abba7670aafe262dcd
sharathkumar66kk-star <sharathkumar66kk@gmail.com>
2026-01-11 18:55:03 +0530

    Add variables demo program

---
+++ basics/variables/VariablesDemo.java
@@ -0,0 +1,11 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 20;
+        double marks = 85.5;
+        char grade = 'A';
+
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(grade);
+    }
+}
