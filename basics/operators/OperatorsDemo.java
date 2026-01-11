45bd24539901237a1db521f5a8f1a42f54fe2018
sharathkumar66kk-star <sharathkumar66kk@gmail.com>
2026-01-11 19:36:55 +0530

    Add operators demo program

---
+++ basics/operators/OperatorsDemo.java
@@ -0,0 +1,14 @@
+
+
+public class OperatorsDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a + b);   // addition
+        System.out.println(a - b);   // subtraction
+        System.out.println(a * b);   // multiplication
+        System.out.println(a / b);   // division
+        System.out.println(a % b);   // modulus
+    }
+}
