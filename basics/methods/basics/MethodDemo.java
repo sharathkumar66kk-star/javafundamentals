480c764b9d527ef56bc2cad065c54e2771d89b8b
sharathkumar66kk-star <sharathkumar66kk@gmail.com>
2026-01-11 20:04:57 +0530

    Add basic method demo

---
+++ basics/methods/basics/MethodDemo.java
@@ -0,0 +1,10 @@
+public class MethodDemo {
+
+    static void greet() {
+        System.out.println("Hello from method");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
