28f4f11477f111674fe5befaa67e640284f94ae9
sharathkumar66kk-star <sharathkumar66kk@gmail.com>
2026-01-11 20:07:37 +0530

    Add method with parameters demo

---
+++ basics/methods/with-parameters/MethodWithParamsDemo.java
@@ -0,0 +1,11 @@
+public class MethodWithParamsDemo {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = add(5, 3);
+        System.out.println(result);
+    }
+}
