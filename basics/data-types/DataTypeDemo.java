ba982b11293b00a417e7f58b6eb2a4d2ad599fe6
sharathkumar66kk-star <sharathkumar66kk@gmail.com>
2026-01-11 19:12:59 +0530

    Add data types demo program

---
+++ basics/data-types/DataTypeDemo.java
@@ -0,0 +1,13 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        int age = 20;
+        double marks = 85.5;
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(grade);
+        System.out.println(passed);
+    }
+}
