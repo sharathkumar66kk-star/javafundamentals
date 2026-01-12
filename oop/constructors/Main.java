bded398f9a9aeec60805f3396e0771949b738879
sharathkumar66kk-star <sharathkumar66kk@gmail.com>
2026-01-12 14:52:59 +0530

    Create Main.java

---
+++ oop/constructors/Main.java
@@ -0,0 +1,10 @@
+package oop.constructors;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student("Alex", 20);
+
+        System.out.println(s.name);
+        System.out.println(s.age);
+    }
+}
